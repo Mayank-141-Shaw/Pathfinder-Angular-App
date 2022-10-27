a100b94e6b11870f08f43fb946bfef75548621f7
Mayank-141-Shaw <mayank141shaw@gmail.com>
2022-10-27 20:30:19 +0530

    Fixed dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,8 +8,10 @@ import { GraphVisaulizerComponent } from './core/components/graph-visaulizer/gra
 import { NodeComponent } from './core/components/node/node.component';
 import { ActionsComponent } from './core/components/actions/actions.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
